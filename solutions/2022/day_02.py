630f43791736e3456dc0467ceb03013cb452fb0e
EddSH1994 <edd@schauman-haigh.co.uk>
2022-12-27 22:19:49 -0800

    Complete day 2 part 1

--- solutions/2022/day_02.py
+++ solutions/2022/day_02.py
@@ -16,19 +16,46 @@ from time_it import time_it # pylint: disable=wrong-import-position
 def problem_1() -> None:
     """Solution to Problem 1"""
 
-    print("Solution 1")
+    score = 0
+
+    with open('./solutions/2022/data/day_02.data', encoding="utf-8") as file:
+        for line in file:
+            moves = line.rstrip().split(' ')
+            player_move = moves[1]
+
+            match player_move:
+                case 'X':
+                    score += 1
+                case 'Y':
+                    score += 2
+                case 'Z':
+                    score += 3
+
+            match moves:
+                case ['A', 'Z'] | ['B', 'A'] | ['C', 'B']:
+                    score += 0
+                case ['A', 'X'] | ['B', 'Y'] | ['C', 'Z']:
+                    score += 3
+                case ['A', 'Y'] | ['B', 'Z'] | ['C', 'X']:
+                    score += 6
+
+    print(score)
+
 
 @time_it
 def problem_2() -> None:
     """Solution to Problem 2"""
 
-    print("Solution 2")
+    with open('./solutions/2022/data/day_02.data', encoding="utf-8") as file:
+        for line in file:
+            print(line)
 
 
 def main() -> None:
     """Print solutions"""
+
     problem_1()
-    problem_2()
+    # problem_2()
 
 if __name__ == '__main__':
     main()
